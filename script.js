96022b56b08970ac928cb632075a67262cca7984
LucaDeeken <168202686+LucaDeeken@users.noreply.github.com>
2024-09-13 15:08:54 +0200

    edit how inputfields behave when being edited and before

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+//grab inputfields and prevent them to turn red before the user enters something; this is supposed to happen after the input
+const inputFields = document.getElementsByTagName("input");
+for (let i = 0; i < inputFields.length; i++) {
+    inputFields[i].classList.add("touched");
+    inputFields[i].addEventListener('blur', () => {
+        inputFields[i].classList.remove("touched"); // Entferne die Klasse "untouched"
+        inputFields[i].classList.add("untouched");      // Füge die Klasse "touched" hinzu
+      });
+};
